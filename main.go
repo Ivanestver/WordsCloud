9413203b9e7565c02a6f28d507eea29acd9d5692
IvanPatrenkov <ivanestver24@gmail.com>
2025-06-18 22:18:28 +0300

    Добавил создание нового опроса

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", root.RootHandle)
-	http.HandleFunc("new_poll", poll.HandleNewPoll)
+	fs := http.FileServer(http.Dir("poll"))
+	http.Handle("/poll/", http.StripPrefix("/poll/", fs))
+	http.HandleFunc("/root", root.RootHandle)
+	http.HandleFunc("/new_poll", poll.HandleNewPoll)
+	http.HandleFunc("/create_poll", poll.HandleCreatePoll)
 
 	log.Fatal(http.ListenAndServe(":50000", nil))
 	fmt.Println("Started listening to the port 50000")
--- poll/poll.go
+++ poll/poll.go
@@ -1,7 +1,16 @@
 package poll
 
-import "net/http"
+import (
+	"net/http"
+	"wordscloud/questionnaire"
+)
 
 func HandleNewPoll(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "poll/new_poll.html")
+}
 
+func HandleCreatePoll(w http.ResponseWriter, req *http.Request) {
+	questionTitle := req.FormValue("question")
+	questionnaire.MakeNewQuestion(questionTitle)
+	http.Redirect(w, req, "/question", http.StatusSeeOther)
 }
